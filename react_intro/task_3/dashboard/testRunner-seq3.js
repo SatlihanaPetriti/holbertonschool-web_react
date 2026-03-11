6f14839a981d16ee937d670a78ee2a884f83e03e
satlihana_p <satlihanapetriti@gmail.com>
2026-03-12 00:34:32 +0100

    test runner

---
+++ react_intro/task_3/dashboard/testRunner-seq3.js
@@ -0,0 +1,16 @@
+﻿import { execSync } from 'child_process';
+
+try {
+  // Run the tests silently
+  execSync('npx jest src/Notifications.spec.js --silent', {
+    stdio: 'inherit',
+    shell: true,
+    timeout: 10000
+  });
+  
+  console.log('OK');
+  process.exit(0);
+} catch (error) {
+  console.log('NOK');
+  process.exit(1);
+}
