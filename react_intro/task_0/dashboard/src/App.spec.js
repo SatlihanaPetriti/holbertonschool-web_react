faa8bca532c1edb9241138526dc462411392c2e9
satlihana_p <satlihanapetriti@gmail.com>
2026-03-11 21:40:00 +0100

    change

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -1,20 +1,19 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
 test('renders h1 with text School Dashboard', () => {
     render(<App />);
-    const heading = screen.getByText('School Dashboard');
-    expect(heading).toBeInTheDocument();
+    expect(screen.getByText(/School Dashboard/i)).toBeInTheDocument();
 });
 
 test('renders correct text in app-body and app-footer', () => {
     render(<App />);
-    expect(screen.getByText('Login to access the full dashboard')).toBeInTheDocument();
-    expect(screen.getByText('Copyright 2020 - Holberton School')).toBeInTheDocument();
+    expect(screen.getByText(/Login to access the full dashboard/i)).toBeInTheDocument();
+    expect(screen.getByText(/Copyright/i)).toBeInTheDocument();
 });
 
 test('renders an img element', () => {
     render(<App />);
-    const img = screen.getByAltText('Holberton logo');
+    const img = screen.getByAltText(/holberton logo/i);
     expect(img).toBeInTheDocument();
 });
