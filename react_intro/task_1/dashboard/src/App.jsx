db9628929cc98447c7e4a73aaf9f6cac697a027c
satlihana_p <satlihanapetriti@gmail.com>
2026-03-11 22:44:50 +0100

    login form

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -18,6 +18,18 @@ const App = () => {
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <form>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" />
+          </div>
+          <button type="submit">OK</button>
+        </form>
       </div>
 
       <div className="App-footer">
