af80ebfeba3888c3e6df668c55fdf92bbcfcacf1
satlihana_p <satlihanapetriti@gmail.com>
2026-03-11 18:40:48 +0100

    task 0

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,21 +1,27 @@
 import logo from './assets/holberton-logo.jpg';
-import './App.css';
+
+
+import './App.css'
 
 const App = () => {
   const date = new Date()
 
+
   return (
     <>
+
       <div className='App-header'>
         <img src={logo} alt="holberton logo" />
         <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </div>
       <div className='App-body'>
 
-        <p>Login to access the full dashboard</p>
+        <p >Login to access the full dashboard</p>
       </div>
+
       <div className='App-footer'>
-        <p>Copyright {date.getFullYear()} - holberton School</p>
+
+        <p >Copyright {date.getFullYear()} - holberton School</p>
       </div>
     </>
   )
