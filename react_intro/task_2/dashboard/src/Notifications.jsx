830948fb102a54bd391cc6c0c187f0a95bc37769
satlihana_p <satlihanapetriti@gmail.com>
2026-03-11 23:26:27 +0100

    button close

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,10 +1,28 @@
 import './Notifications.css';
-const Notifications = () => {
+import close from './assets/close-button.png';
+import { getLatestNotification } from './utils';
+
+const Notification = () => {
   return (
-      <div className='notification-items'>
-          <p>Here is the list of notifications</p>
+    <div className="notification-items">
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+
+      <button
+        aria-label="Close"
+        style={{ position: "absolute", top: "15px", right: "15px", border: "none", background: "none", cursor: "pointer" }}
+      >
+        <img src={close} style={{ width: "15px", height: "15px" }} alt="close icon" />
+      </button>
     </div>
   );
 };
 
-export default Notifications;
+export default Notification;
