1878de790d9937c94083e020264c31f7046066ed
satlihana_p <satlihanapetriti@gmail.com>
2026-03-11 23:35:36 +0100

    add changes

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,28 +1,29 @@
-import './Notifications.css';
-import close from './assets/close-button.png';
-import { getLatestNotification } from './utils';
+import './Notifications.css'
+import close from './assets/close-button.png'
+import { getLatestNotification } from './utils'
 
 const Notification = () => {
-  return (
-    <div className="notification-items">
-      <p>Here is the list of notifications</p>
-      <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent">New resume available</li>
-        <li
-          data-priority="urgent"
-          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
-        ></li>
-      </ul>
+  return <div className="notification-items   ">
 
-      <button
-        aria-label="Close"
-        style={{ position: "absolute", top: "15px", right: "15px", border: "none", background: "none", cursor: "pointer" }}
-      >
-        <img src={close} style={{ width: "15px", height: "15px" }} alt="close icon" />
-      </button>
-    </div>
-  );
-};
+    <p>Here is the list of notifications</p>
+    <ul>
+      <li data-priority="default">
+        New course available
+      </li>
+      <li data-priority="urgent">
+        New resume available
+      </li>
+      <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}>
 
+      </li>
+
+
+    </ul>
+    <button aria-label='Close' style={{ position: "absolute", top: "10px", right: "10px" }} onClick={() => { console.log('Close button has been clicked to the console.') }} >
+
+      <img src={close} style={{ width: '30px', height: '30px' }} />
+    </button>
+
+  </div>
+}
 export default Notification;
