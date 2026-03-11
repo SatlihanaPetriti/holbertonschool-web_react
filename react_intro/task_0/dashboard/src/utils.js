684adbb42ceb87cbf52e215fe2791cf4c36c509c
satlihana_p <satlihanapetriti@gmail.com>
2026-03-11 18:00:29 +0100

    spec added

---
+++ react_intro/task_0/dashboard/src/utils.js
@@ -0,0 +1,14 @@
+export function getCurrentYear() {
+    const date = new Date()
+    return date.getFullYear();
+
+}
+
+export function getFooterCopy(isIndex) {
+    if (isIndex === true) {
+        return 'Holberton School'
+    } else {
+        return 'Holberton School main dashboard'
+    }
+
+}
