6ef3b546ec0ca87b73183b320f562a7827d7dc8d
satlihana_p <satlihanapetriti@gmail.com>
2026-03-13 12:26:47 +0100

    change

--- react_props/task_0/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import Notifications from './Notifications';
 
-test('Header renders without crashing', () => {
+test('Notifications renders without crashing', () => {
     render(<Notifications />);
 });
