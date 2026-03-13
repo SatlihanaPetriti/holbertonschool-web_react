448ed12864a3433d8d048ab6edd36a4cb3a880ff
satlihana_p <satlihanapetriti@gmail.com>
2026-03-13 12:28:58 +0100

    cod notifi

--- react_props/task_0/dashboard/src/Notifications/Notifications.test.js
+++
@@ -1,6 +0,0 @@
-import { render } from '@testing-library/react';
-import Notifications from './Notifications';
-
-test('Notifications renders without crashing', () => {
-    render(<Notifications />);
-});
