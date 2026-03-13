f155b43cb171375950883b1add47378c790795d4
satlihana_p <satlihanapetriti@gmail.com>
2026-03-13 16:26:58 +0100

    check

--- react_props/task_1/dashboard/src/Header/Header.spec.js
+++ react_props/task_1/dashboard/src/Header/Header.spec.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
-describe('Header Component', () => {
+describe('Header', () => {
     test('renders the Holberton logo', () => {
         render(<Header />);
-        const logo = screen.getByAltText(/holberton logo/i);
+        const logo = screen.getByAltText('holberton logo');
         expect(logo).toBeInTheDocument();
     });
 
-    test('renders h1 heading with correct text', () => {
+    test('renders the heading h1 element with the correct text', () => {
         render(<Header />);
         const heading = screen.getByRole('heading', { level: 1 });
         expect(heading).toHaveTextContent('School dashboard');
