1112b8ef4aa885c5e755095b6837d05ba9a6384d
satlihana_p <satlihanapetriti@gmail.com>
2026-03-13 10:29:42 +0100

    change

---
+++ react_intro/task_5/dashboard/src/App/App.spec.js
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders h1 with School Dashboard text", () => {
+    render(<App />);
+    expect(screen.getByRole('heading')).toHaveTextContent(/School dashboard/i);
+});
+
+test("renders correct text in body and footer paragraphs", () => {
+    render(<App />);
+    expect(screen.getByText(/Login to access the full dashboard/i)).toBeInTheDocument();
+    expect(screen.getByText(/Copyright/i)).toBeInTheDocument();
+});
+
+test("renders an image", () => {
+    render(<App />);
+    const image = screen.getByAltText(/holberton logo/i);
+    expect(image).toBeInTheDocument();
+});
+
+test("renders 2 labels Email and Password", () => {
+    render(<App />);
+    const labelEmail = screen.getByText(/email/i);
+    const labelPassword = screen.getByText(/password/i);
+
+    expect(labelEmail).toBeInTheDocument();
+    expect(labelPassword).toBeInTheDocument();
+});
+
+test("renders 2 input elements (email and password)", () => {
+    render(<App />);
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+});
+
+test("renders a button with OK text", () => {
+    render(<App />);
+    const button = screen.getByRole('button');
+    expect(button).toHaveTextContent(/OK/i);
+    expect(button).toBeInTheDocument();
+});
