7ab8ea92f0807e8e5bcae4888cccca57762f37f0
satlihana_p <satlihanapetriti@gmail.com>
2026-03-13 09:38:27 +0100

    chnges

--- react_intro/task_4/dashboard/src/Notifications.jsx
+++ react_intro/task_4/dashboard/src/Notifications.jsx
@@ -16,9 +16,9 @@ function Notifications() {
             <p>Here is the list of notifications</p>
 
             <ul>
-                <li data-priority="default" className="blue">New course available</li>
-                <li data-priority="urgent" className="red">New resume available</li>
-                <li data-priority="urgent" className="red" >
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent">
                     <strong>Urgent requirement</strong> - complete by EOD
                 </li>
             </ul>
