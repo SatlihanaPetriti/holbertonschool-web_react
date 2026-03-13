aec162cc78e681bc033ccc2335d62870043811c6
satlihana_p <satlihanapetriti@gmail.com>
2026-03-13 11:55:22 +0100

    task_0 props

---
+++ react_props/task_0/dashboard/src/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React from 'react';
+import './Footer.css';
+import { getCurrentYear, getFooterCopy } from '../utils/utils';
+
+function Footer() {
+    return (
+        <div className="App-footer">
+            <p>
+                Copyright {getCurrentYear()} - {getFooterCopy(false)}
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
