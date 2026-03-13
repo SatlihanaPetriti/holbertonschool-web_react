55134a521278bf2848883a3e8114ee30736fdf0f
satlihana_p <satlihanapetriti@gmail.com>
2026-03-13 12:44:19 +0100

    spec update

--- react_props/task_0/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_0/dashboard/src/Footer/Footer.spec.js
@@ -1,6 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Footer from './Footer';
+import { getCurrentYear } from '../utils/utils';
 
-test('Footer renders without crashing', () => {
-    render(<Footer />);
+describe('Footer Component', () => {
+    test('renders copyright with current year when isIndex=true', () => {
+        const year = getCurrentYear();
+        render(<Footer />);
+
+        const paragraph = screen.getByText(
+            `Copyright ${year} - Holberton School main dashboard`,
+            { exact: false }
+        );
+
+        expect(paragraph).toBeInTheDocument();
+    });
 });
--- react_props/task_0/dashboard/src/Header/Header.spec.js
+++ react_props/task_0/dashboard/src/Header/Header.spec.js
@@ -1,6 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
-test('Header renders without crashing', () => {
-    render(<Header />);
+describe('Header Component', () => {
+    beforeEach(() => {
+        render(<Header />);
+    });
+
+    test('renders the Holberton logo', () => {
+        const logo = screen.getByAltText(/holberton logo/i);
+        expect(logo).toBeInTheDocument();
+    });
+
+    test('renders the heading h1 with correct text', () => {
+        const heading = screen.getByRole('heading', { level: 1 });
+        expect(heading).toHaveTextContent('School dashboard');
+    });
 });
