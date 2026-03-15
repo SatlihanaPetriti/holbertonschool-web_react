c372c56497454499aea1e6ecf13fe9f7f4ca9378
satlihana_p <satlihanapetriti@gmail.com>
2026-03-15 22:05:21 +0100

    add bodysecion component

--- react_component/task_3/dashboard/src/App/App.jsx
+++ react_component/task_3/dashboard/src/App/App.jsx
@@ -1,98 +1,86 @@
-/* eslint-disable */
-import { Component } from 'react';
-import CourseList from '../CourseList/CourseList'
-import '../CourseList/CourseList.css'
-import Notifications from '../Notifications/Notifications';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import { getLatestNotification } from '../utils/utils';
-import './App.css'
+import { Component } from "react";
+import CourseList from "../CourseList/CourseList";
+import "../CourseList/CourseList.css";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Notifications from "../Notifications/Notifications";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import "./App.css";
 
-// function App({ isLoggedIn = false }) {
-//   const notificationsList = [
-//     {
-//       id: 1,
-//       type: 'default',
-//       value: 'New course available'
-//     },
-//     {
-//       id: 2,
-//       type: 'urgent',
-//       value: 'New resume available'
-//     },
-//     {
-//       id: 3,
-//       type: 'urgent',
-//       html: { __html: getLatestNotification() }
-//     }
-//   ];
-//   const coursesList = [
-//     { id: 1, name: "ES6", credit: "60" },
-//     { id: 2, name: "Webpack", credit: "20" },
-//     { id: 3, name: "React", credit: "40" },
-//   ];
-
-//   return (
-//     <>
-//       <div className="root-notifications">
-//         <Notifications notifications={notificationsList} displayDrawer={true} />
-//       </div>
-//       <Header />
-//       {isLoggedIn ? (<div className='courses-body'><CourseList courses={coursesList} /></div>) : (<Login />)}
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.notificationsList = [
-      {
-        id: 1,
-        type: 'default',
-        value: 'New course available'
-      },
-      {
-        id: 2,
-        type: 'urgent',
-        value: 'New resume available'
-      },
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
       {
         id: 3,
-        type: 'urgent',
-        html: { __html: getLatestNotification() }
-      }
+        type: "urgent",
+        html: { __html: "<strong>Urgent requirement</strong> - complete by EOD" },
+      },
     ];
+
     this.coursesList = [
       { id: 1, name: "ES6", credit: "60" },
       { id: 2, name: "Webpack", credit: "20" },
       { id: 3, name: "React", credit: "40" },
     ];
+
+    this.handleKeyDown = (e) => {
+      if (e.ctrlKey && e.key === "h") {
+        e.preventDefault();
+        alert("Logging you out");
+        this.props.logOut();
+      }
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
   }
 
   render() {
-    const { isLoggedIn = false } = this.props;
+    const { isLoggedIn } = this.props;
 
     return (
       <>
-        <div className="root-notifications">
-          <Notifications notifications={this.notificationsList} displayDrawer={true} />
-        </div>
-        <Header />
-        {isLoggedIn ? (
-          <div className='courses-body'>
-            <CourseList courses={this.coursesList} />
+        <div className="notifications-header">
+          <Header />
+          <div className="root-notifications">
+            <Notifications notifications={this.notificationsList} />
           </div>
+        </div>
+
+        {isLoggedIn ? (
+          <BodySectionWithMarginBottom title="Course list">
+            <CourseList courses={this.coursesList} />
+          </BodySectionWithMarginBottom>
         ) : (
-          <Login />
+          <BodySectionWithMarginBottom title="Log in to continue">
+            <Login />
+          </BodySectionWithMarginBottom>
         )}
+
+        <BodySection title="News from the School">
+          <p>Holberton School News goes here</p>
+        </BodySection>
+
         <Footer />
       </>
     );
   }
 }
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => { },
+};
 
 export default App;
