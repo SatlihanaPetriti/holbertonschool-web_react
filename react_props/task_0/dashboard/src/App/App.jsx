eee181dd94a481938f19372a7b7dbdf444b3b222
satlihana_p <satlihanapetriti@gmail.com>
2026-03-15 15:41:18 +0100

    chnaeg

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,22 +1,20 @@
-/* eslint-disable */
-import { Fragment } from 'react';
-import Notifications from '../Notifications/Notifications';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+import Login from '../Login/Login'
+import Notifications from '../Notifications/Notifications'
 import './App.css'
 
 function App() {
   return (
-    <Fragment>
-      <div className="root-notifications">
+    <>
+      <div className='root-notifications'>
         <Notifications />
       </div>
       <Header />
       <Login />
       <Footer />
-    </Fragment>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
