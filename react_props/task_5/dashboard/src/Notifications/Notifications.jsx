e009fc3811bc7262049efbb7fe4685996a3037f6
satlihana_p <satlihanapetriti@gmail.com>
2026-03-15 19:35:34 +0100

    notificatiob

--- react_props/task_5/dashboard/src/Notifications/Notifications.jsx
+++ react_props/task_5/dashboard/src/Notifications/Notifications.jsx
@@ -1,28 +1,51 @@
 /* eslint-disable */
+import React from "react";
 import closeIcon from "../assets/close-button.png";
-import NotificationItem from './NotificationItem';
-import './Notifications.css';
+import NotificationItem from "./NotificationItem";
+import "./Notifications.css";
 
- const Notifications= ({ notifications = [], displayDrawer = false })=>{
+const Notifications = ({ notifications = [], displayDrawer = false }) => {
     return (
-        <>
-            <div className='notifications-title'>Your notifications</div>
-            {displayDrawer ? (<div className="notifications">
-                <div>
-                    <p>Here is the list of notifications</p>
-                    <ul>
-                        {notifications.length > 0 ? ((notifications).map(notification => {
-                            return (
-                                <NotificationItem key={notification.id} type={notification.type} html={notification.html} value={notification.value} />
-                            )
-                        })) : <p>No new notification for now</p>}
-                    </ul>
+        <div className="notifications-container">
+            {/* Always visible title */}
+            <div className="notification-title">Your notifications</div>
+
+            {/* Only render drawer if displayDrawer is true */}
+            {displayDrawer && (
+                <div className="notification-items">
+                    {/* Message above the list */}
+                    {notifications.length > 0 ? (
+                        <p>Here is the list of notifications</p>
+                    ) : (
+                        <p>No new notification for now</p>
+                    )}
+
+                    {/* Notification list */}
+                    {notifications.length > 0 && (
+                        <ul>
+                            {notifications.map((notification) => (
+                                <NotificationItem
+                                    key={notification.id}
+                                    type={notification.type}
+                                    html={notification.html}
+                                    value={notification.value}
+                                />
+                            ))}
+                        </ul>
+                    )}
+
+                    {/* Close button */}
+                    <button
+                        aria-label="Close"
+                        onClick={() => console.log("Close button has been clicked")}
+                        className="close-button"
+                    >
+                        <img alt="Close Button" src={closeIcon} />
+                    </button>
                 </div>
-                <button aria-label="Close" onClick={() => { console.log("Close button has been clicked") }} style={{ marginLeft: "auto", backgroundColor: "white", border: "none", height: "25px" }}>
-                    <img alt="Close Button" src={closeIcon}></img>
-                </button>
-            </div>) : (null)}
-        </>
-    )
-}
+            )}
+        </div>
+    );
+};
+
 export default Notifications;
