d209fe2e128a529dc9b49ad01de4d2619a3a1909
satlihana_p <satlihanapetriti@gmail.com>
2026-04-09 16:57:43 +0200

    refactor: update App and Login components for improved state management and structure

--- react_hooks/task_0/dashboard/src/App/App.jsx
+++ react_hooks/task_0/dashboard/src/App/App.jsx
@@ -3,48 +3,55 @@ import CourseList from "../CourseList/CourseList";
 import "../CourseList/CourseList.css";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import Login from "../Login/Login";
+import { Login } from "../Login/Login";
 import Notifications from "../Notifications/Notifications";
 import BodySection from "../BodySection/BodySection";
 import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
-import AppContext from "../Context/context";
 import "./App.css";
-
-const notificationsList = [
-  { id: 1, type: "default", value: "New course available" },
-  { id: 2, type: "urgent", value: "New resume available" },
-  {
-    id: 3,
-    type: "urgent",
-    html: {
-      __html: "<strong>Urgent requirement</strong> - complete by EOD",
-    },
-  },
-];
-
-const coursesList = [
-  { id: 1, name: "ES6", credit: "60" },
-  { id: 2, name: "Webpack", credit: "20" },
-  { id: 3, name: "React", credit: "40" },
-];
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      displayDrawer: true,
-      user: { email: "", password: "", isLoggedIn: false },
-      notifications: notificationsList,
-      courses: coursesList,
+      displayDrawer: false,
+      isLoggedIn: false,
     };
 
     this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
     this.handleHideDrawer = this.handleHideDrawer.bind(this);
-    this.logIn = this.logIn.bind(this);
-    this.logOut = this.logOut.bind(this);
-    this.markNotificationAsRead =
-      this.markNotificationAsRead.bind(this);
+
+    this.notificationsList = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      {
+        id: 3,
+        type: "urgent",
+        html: { __html: "<strong>Urgent requirement</strong> - complete by EOD" },
+      },
+    ];
+
+    this.coursesList = [
+      { id: 1, name: "ES6", credit: "60" },
+      { id: 2, name: "Webpack", credit: "20" },
+      { id: 3, name: "React", credit: "40" },
+    ];
+
+    this.handleKeyDown = (e) => {
+      if (e.ctrlKey && e.key === "h") {
+        e.preventDefault();
+        alert("Logging you out");
+        this.props.logOut();
+      }
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
   }
 
   handleDisplayDrawer() {
@@ -55,57 +62,34 @@ class App extends Component {
     this.setState({ displayDrawer: false });
   }
 
-  logIn(email, password) {
-    this.setState({
-      user: { email, password, isLoggedIn: true },
-    });
-  }
-
-  logOut() {
-    this.setState({
-      user: { email: "", password: "", isLoggedIn: false },
-    });
-  }
-
-  markNotificationAsRead(id) {
-    console.log(`Notification ${id} has been marked as read`);
-
-    this.setState((prevState) => ({
-      notifications: prevState.notifications.filter((n) => n.id !== id),
-    }));
-  }
+  handleLogin = () => {
+    this.setState({ isLoggedIn: true });
+  };
 
   render() {
-    const { user, displayDrawer, notifications, courses } = this.state;
-
-    const contextValue = {
-      user,
-      logOut: this.logOut,
-    };
+    const { isLoggedIn } = this.state;
 
     return (
-      <AppContext.Provider value={contextValue}>
+      <>
         <div className="notifications-header">
           <Header />
-
           <div className="root-notifications">
             <Notifications
-              notifications={notifications}
-              displayDrawer={displayDrawer}
+              notifications={this.notificationsList}
+              displayDrawer={this.state.displayDrawer}
               handleDisplayDrawer={this.handleDisplayDrawer}
               handleHideDrawer={this.handleHideDrawer}
-              markNotificationAsRead={this.markNotificationAsRead}
             />
           </div>
         </div>
 
-        {user.isLoggedIn ? (
+        {isLoggedIn ? (
           <BodySectionWithMarginBottom title="Course list">
-            <CourseList courses={courses} />
+            <CourseList courses={this.coursesList} />
           </BodySectionWithMarginBottom>
         ) : (
           <BodySectionWithMarginBottom title="Log in to continue">
-            <Login logIn={this.logIn} />
+            <Login onLogin={this.handleLogin} />
           </BodySectionWithMarginBottom>
         )}
 
@@ -114,9 +98,14 @@ class App extends Component {
         </BodySection>
 
         <Footer />
-      </AppContext.Provider>
+      </>
     );
   }
 }
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => { },
+};
 
 export default App;
--- react_hooks/task_0/dashboard/src/Login/Login.jsx
+++ react_hooks/task_0/dashboard/src/Login/Login.jsx
@@ -1,88 +1,22 @@
 /* eslint-disable */
-import React, { Component } from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "./Login.css";
-import WithLogging from "../HOC/WithLogging";
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
+export function Login({ onLogin }) {
+    return (
+        <div>
+            <p>Login to access the full dashboard</p>
 
-        this.state = {
-            email: props.email || "",
-            password: props.password || "",
-            enableSubmit: false,
-        };
+            <label htmlFor="email">Email: </label>
+            <input type="email" id="email" />
 
-        this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
-        this.handleChangeEmail = this.handleChangeEmail.bind(this);
-        this.handleChangePassword = this.handleChangePassword.bind(this);
-    }
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" />
 
-    validateForm(email, password) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        return emailRegex.test(email) && password.length >= 8;
-    }
-
-    handleChangeEmail(e) {
-        const email = e.target.value;
-
-        this.setState((prevState) => ({
-            email,
-            enableSubmit: this.validateForm(email, prevState.password),
-        }));
-    }
-
-    handleChangePassword(e) {
-        const password = e.target.value;
-
-        this.setState((prevState) => ({
-            password,
-            enableSubmit: this.validateForm(prevState.email, password),
-        }));
-    }
-
-    handleLoginSubmit(e) {
-        e.preventDefault();
-
-        const { email, password } = this.state;
-
-        if (this.props.logIn) {
-            this.props.logIn(email, password);
-        }
-    }
-
-    render() {
-        const { email, password, enableSubmit } = this.state;
-
-        return (
-            <div className="App-body">
-                <p>Login to access the full dashboard</p>
-
-                <form onSubmit={this.handleLoginSubmit}>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        value={email}
-                        onChange={this.handleChangeEmail}
-                    />
-
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={this.handleChangePassword}
-                    />
-
-                    <input type="submit" value="OK" disabled={!enableSubmit} />
-                </form>
-            </div>
-        );
-    }
+            <button type="button" onClick={onLogin}>
+                OK
+            </button>
+        </div>
+    );
 }
-
-export default WithLogging(Login);
