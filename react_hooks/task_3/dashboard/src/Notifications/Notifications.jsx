266f89bd377789378e8218ae2296ae6801b6402e
satlihana_p <satlihanapetriti@gmail.com>
2026-04-14 15:41:02 +0200

    memo

--- react_hooks/task_3/dashboard/src/Notifications/Notifications.jsx
+++ react_hooks/task_3/dashboard/src/Notifications/Notifications.jsx
@@ -1,67 +1,72 @@
-import { memo } from "react";
+/* eslint-disable */
+import React from "react";
 import closeIcon from "../assets/close-button.png";
 import NotificationItem from "./NotificationItem";
 import "./Notifications.css";
 
-function Notifications({
-    notifications = [],
-    displayDrawer = false,
-    handleDisplayDrawer,
-    handleHideDrawer,
-}) {
-    const markAsRead = (id) => {
+class Notifications extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        return (
+            nextProps.notifications.length !== this.props.notifications.length ||
+            nextProps.displayDrawer !== this.props.displayDrawer
+        );
+    }
+
+    markAsRead = (id) => {
         console.log(`Notification ${id} has been marked as read`);
     };
 
-    return (
-        <div className="notifications-container">
-            <div
-                className="notification-title"
-                onClick={handleDisplayDrawer}
-            >
-                Your notifications
-            </div>
+    render() {
+        const {
+            notifications = [],
+            displayDrawer = false,
+            handleDisplayDrawer,
+            handleHideDrawer,
+        } = this.props;
 
-            {displayDrawer && (
-                <div className="notification-items">
-                    {notifications.length > 0 ? (
-                        <>
-                            <p>Here is the list of notifications</p>
-                            <ul>
-                                {notifications.map((notification) => (
-                                    <NotificationItem
-                                        key={notification.id}
-                                        id={notification.id}
-                                        type={notification.type}
-                                        html={notification.html}
-                                        value={notification.value}
-                                        markAsRead={markAsRead}
-                                    />
-                                ))}
-                            </ul>
-                        </>
-                    ) : (
-                        <p>No new notification for now</p>
-                    )}
-
-                    <button
-                        aria-label="Close"
-                        onClick={handleHideDrawer}
-                        className="close-button"
-                    >
-                        <img alt="Close Button" src={closeIcon} />
-                    </button>
+        return (
+            <div className="notifications-container">
+                <div
+                    className="notification-title"
+                    onClick={handleDisplayDrawer}
+                >
+                    Your notifications
                 </div>
-            )}
-        </div>
-    );
+
+                {displayDrawer && (
+                    <div className="notification-items">
+                        {notifications.length > 0 ? (
+                            <>
+                                <p>Here is the list of notifications</p>
+                                <ul>
+                                    {notifications.map((notification) => (
+                                        <NotificationItem
+                                            key={notification.id}
+                                            id={notification.id}
+                                            type={notification.type}
+                                            html={notification.html}
+                                            value={notification.value}
+                                            markAsRead={this.markAsRead}
+                                        />
+                                    ))}
+                                </ul>
+                            </>
+                        ) : (
+                            <p>No new notification for now</p>
+                        )}
+
+                        <button
+                            aria-label="Close"
+                            onClick={handleHideDrawer}
+                            className="close-button"
+                        >
+                            <img alt="Close Button" src={closeIcon} />
+                        </button>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
-function areEqual(prevProps, nextProps) {
-    if (prevProps.displayDrawer !== nextProps.displayDrawer) return false;
-    if (prevProps.notifications.length !== nextProps.notifications.length)
-        return false;
-    return true;
-}
-
-export default memo(Notifications, areEqual);
+export default Notifications;
