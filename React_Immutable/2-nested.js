f76941ae7257e9c44fcd111641dc17f6acf411fa
satlihana_p <satlihanapetriti@gmail.com>
2026-04-15 11:24:15 +0200

    nested

---
+++ React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+/*eslint-disable*/
+import { fromJS } from "immutable";
+
+export default function accessImmutableObject(object, array) {
+    const immutableObj = fromJS(object);
+    return immutableObj.getIn(array);
+}
