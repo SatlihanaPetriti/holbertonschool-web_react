961cfb47655103e4856d99b9a20c732e62ec0545
satlihana_p <satlihanapetriti@gmail.com>
2026-04-15 10:17:09 +0200

    chandfge

--- React_Immutable/0-fromjs.js
+++ React_Immutable/0-fromjs.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable';
 
 function getImmutableObject(object) {
-    const map = fromJS(object)
+    const map = fromJS(object);
     return map;
 }
 export default getImmutableObject;
