d532cf15b922a508ad5408f7a3523d4423e863f5
satlihana_p <satlihanapetriti@gmail.com>
2026-04-15 11:30:17 +0200

    change

--- React_Immutable/2-nested.js
+++ React_Immutable/2-nested.js
@@ -1,7 +1,6 @@
 /*eslint-disable*/
 import { fromJS } from "immutable";
 
-export default function accessImmutableObject(object, array) {
-    const immutableObj = fromJS(object);
-    return immutableObj.getIn(array);
+export default function getImmutableObject(object) {
+    return fromJS(object);
 }
